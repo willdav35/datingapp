0029a4a316f2ffc171faf46388fbf7836b1119f3
William Davis <willdav35@protonmail.com>
2022-06-03 13:06:54 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+            services.AddMvcCore();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -53,8 +55,11 @@ namespace API
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
+
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            
+            
 
             app.UseAuthorization();
 
